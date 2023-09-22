a20bbd9287f7be0b3b0841871215651ff41a6197
Simona Cartolina <simonacartolina@hotmail.com>
2023-09-22 11:01:51 +0200

    made searchBar working

--- src/components/pages/restaurantsList.vue
+++ src/components/pages/restaurantsList.vue
@@ -3,8 +3,8 @@
     <template>
         <div class="col-lg-6 col-md-3 col-sm-11 order-lg-1 order-md-1 order-sm-2 my-sm-2">
         <div class="input-group">
-            <input v-model="searchBar" @keyup.enter="newSearch(searchTerm)" id="searchBar" type="search" class="form-control px-2 py-2" placeholder="Ristoranti, spesa, piatti" style="width: 620px; background-color: #f5f5f5; color: #cccdcd" />
-            <button @click="newSearch(searchTerm)"> search </button>
+            <input v-model="searchBar" @keyup.enter="newSearch(searchBar)" id="searchBar" type="search" class="form-control px-2 py-2" placeholder="Ristoranti, spesa, piatti" style="width: 620px; background-color: #f5f5f5; color: #cccdcd" />
+            <button @click="newSearch(searchBar)"> search </button>
 
 
         </div>
@@ -35,10 +35,14 @@
         };
         },
             methods: {
-            getRestaurants() {
-                axios
-                .get(this.apiUrl)
-                .then((response) => {
+
+
+            getRestaurants(apiUrl=this.apiUrl, nameQuery=false){
+                const params= {}
+                if(nameQuery){
+                    params.search=nameQuery;
+                }
+                axios.get(apiUrl, {params}).then((response) => {
                     console.log(response.data);
                     this.restaurantsList = response.data;
                 })
@@ -46,18 +50,21 @@
                     console.log(error);
                 });
             },
-            newSearch(searchTerm) {
-                this.apiUrl = `http://localhost:8000/api/restaurants?search=${searchTerm}`;
-                console.log('Api URL:', this.apiUrl);
-                this.getRestaurants();
-                this.restaurantsList = [];
+
+            newSearch(searchBar) {
+                this.getRestaurants(this.apiUrl, searchBar);
+                //  = `http://localhost:8000/api/restaurants?search=${searchBar}`;
+                console.log('Api URL:', this.restaurantsList);
+                // this.getRestaurants();
+                // this.restaurantsList = [];
 },
 
 
 
     },
     created() {
-        this.getRestaurants();
+        this.getRestaurants(this.apiUrl);
+        
         },
     };
     </script>
