f32cc6f55dd3222136d215c7dc3a2f25a96497f2
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-09-25 11:26:32 +0200

    change main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import { createApp } from 'vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import App from './App.vue'
-import 'font-awesome/scss/font-awesome.scss'
 
 createApp(App).mount('#app')
