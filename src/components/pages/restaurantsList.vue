61653c70ccad4c8d379bcb23823b382439c10baa
Simona Cartolina <simonacartolina@hotmail.com>
2023-09-26 12:13:09 +0200

    made navbar responsive

--- src/components/pages/restaurantsList.vue
+++ src/components/pages/restaurantsList.vue
@@ -1,13 +1,17 @@
     
     
     <template>
-        <div class="col-lg-6 col-md-3 col-sm-11 order-lg-1 order-md-1 order-sm-2 my-sm-2">
+        <div class="col-lg-6 col-md-3 col-sm-11 order-lg-1 order-md-1 order-sm-2 my-sm-2 d-flex align-items-center pt-2">
         <div class="input-group">
             <input v-model="searchBar" @keyup.enter="newSearch(searchBar)" id="searchBar" type="search" class="form-control px-2 py-2" placeholder="Ristoranti, spesa, piatti" style="width: 620px; background-color: #f5f5f5; color: #cccdcd" />
-            <button @click="newSearch(searchBar)"> search </button>
-
-
         </div>
+        <button @click="newSearch(searchBar)" class=" px-3 py-2"> 
+        
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+            </svg>
+
+        </button>
         </div>
         <div class="mycontainer py-5 restaurants d-flex flex-wrap">
         <div v-for="restaurant in restaurantsList" :key="restaurant.id" class="row mx-4 my-4">
