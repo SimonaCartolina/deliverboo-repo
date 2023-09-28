661a2d8e4d63cccfe80dd41c695549a10094bb51
Simona Cartolina <simonacartolina@hotmail.com>
2023-09-28 21:42:05 +0200

    corrcted soft main

--- src/components/pages/SingleRestaurant.vue
+++ src/components/pages/SingleRestaurant.vue
@@ -2,69 +2,69 @@
 
 import axios from "axios";
 export default {
-  name: "SingleRestaurant",
-  data(){
+    name: "SingleRestaurant",
+    data(){
     return{
 
-      singleRestaurant: [],
-      cart: [], 
-      restaurantMenu: [],
-      apiUrl:"",
-      tip: 0,
+        singleRestaurant: [],
+        cart: [], 
+        restaurantMenu: [],
+        apiUrl:"",
+        tip: 0,
     }
-  },
+    },
 
-  computed: {
+    computed: {
     totalPrice() {
-      return this.cart.reduce((total, item) => {
+        return this.cart.reduce((total, item) => {
         return total + item.plate.price * item.quantity;
-      }, 0);
+        }, 0);
     },
 
     deliveryFee() {
       return this.totalPrice * 0.02;
     },
-  },
+    },
 
-  methods: {
+    methods: {
 
     getSingleRestaurant() {
 
-      const id = this.$route.params.id;
+        const id = this.$route.params.id;
 
       // Construct the apiUrl with the 'id'
-      this.apiUrl = `http://localhost:8000/api/restaurants/restaurant/${id}`;
-      axios
+        this.apiUrl = `http://localhost:8000/api/restaurants/restaurant/${id}`;
+        axios
         .get(this.apiUrl)
         .then((response) => {
-          console.log(response.data);
-          this.singleRestaurant = response.data;
+        console.log(response.data);
+        this.singleRestaurant = response.data;
         })
         .catch(function (error) {
-          console.log(error);
+        console.log(error);
         });
     },
 
     addToCart(plate) {
-      const existingItem = this.cart.find((item) => item.plate.id === plate.id)
-      
-      ;
-      if (existingItem) {
-              existingItem.quantity++;
+    const existingItem = this.cart.find((item) => item.plate.id === plate.id)
+
+    ;
+    if (existingItem) {
+            existingItem.quantity++;
               console.log(existingItem); // Aumenta la quantità se il piatto è già nel carrello
-      } else {
-              this.cart.push({ plate, quantity: 1 });
+    } else {
+            this.cart.push({ plate, quantity: 1 });
               console.log(plate) // Aggiungi il piatto al carrello
-      }
-      },
+    }
+    },
                         
-      removeFromCart(plate) {
+        removeFromCart(plate) {
         const index = this.cart.findIndex(item => item.plate.id === plate.plate.id);
 
         if (index !== -1) {
           this.cart.splice(index, 1); // Remove the plate from the cart
         }
-      },
+        },
 
 
 
@@ -74,15 +74,15 @@ export default {
 
         decreaseQuantity(plate) {
             if (plate.quantity > 1) {
-              plate.quantity--;
-          }
+                plate.quantity--;
+            }
+            },
+            
         },
         
-      },
-      
-      created() {
-        this.getSingleRestaurant();
-      }
+        created() {
+            this.getSingleRestaurant();
+        }
 
 };
 
@@ -91,60 +91,52 @@ export default {
 
 <template>
 
-<<<<<<< HEAD
-  <div class="SingleRestaurant d-flex flex-wrap m-5">
-=======
-  <div class="SingleRestaurant d-flex m-5 mb-5">
->>>>>>> carrello
-
-            <div class="immagine-profile col-lg-6 col-md-6 col-sm-12">
-
-              <img v-if="singleRestaurant.restaurant.image.startsWith('http')" :src="singleRestaurant.restaurant.image" alt="immagine">
-                <img v-else :src="'http://127.0.0.1:8000/storage/' + singleRestaurant.restaurant.image" alt="immagine">
-                <!-- <img :src="singleRestaurant.restaurant.image" :alt="singleRestaurant.restaurant.id"> -->
-
-            </div>
+    <div class="SingleRestaurant d-flex flex-wrap m-5">
 
 
-            <div class="container-profile col-lg-3 col-md-3 col-sm-12 py-5">
-                <h2 class="card-title fw-bolder" style="color: #262c2cf7">{{ singleRestaurant.restaurant.name  }}</h2>
 
-                <p class="card-text fw-bold mt-3 fs-3" style="color:#618B35"> {{ singleRestaurant.restaurant.address  }}</p>
+                <div class="immagine-profile col-lg-6 col-md-6 col-sm-12">
 
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item fw-bold" style="color:#618B35">{{ singleRestaurant.restaurant.opening_time  }}</li>
-                    <li class="list-group-item fw-bold" style="color:#618B35">{{ singleRestaurant.restaurant.category }}</li>
-                </ul>
-              </div>
-  </div>
+                <img v-if="singleRestaurant.restaurant.image.startsWith('http')" :src="singleRestaurant.restaurant.image" alt="immagine">
+                    <img v-else :src="'http://127.0.0.1:8000/storage/' + singleRestaurant.restaurant.image" alt="immagine">
+                    <!-- <img :src="singleRestaurant.restaurant.image" :alt="singleRestaurant.restaurant.id"> -->
 
-  <h1 class="fw-bolder text-center" style="color:#00CCBC">
-    OUR MENU:
-  </h1>
-  <div class="menu d-flex flex-wrap col-12">
+                </div>
+
+
+                <div class="container-profile col-lg-3 col-md-3 col-sm-12 py-5">
+                    <h2 class="card-title fw-bolder" style="color: #262c2cf7">{{ singleRestaurant.restaurant.name  }}</h2>
+
+                    <p class="card-text fw-bold mt-3 fs-3" style="color:#618B35"> {{ singleRestaurant.restaurant.address  }}</p>
+
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item fw-bold" style="color:#618B35">{{ singleRestaurant.restaurant.opening_time  }}</li>
+                        <li class="list-group-item fw-bold" style="color:#618B35">{{ singleRestaurant.restaurant.category }}</li>
+                    </ul>
+                </div>
+    </div>
+
+    <h1 class="fw-bolder text-center" style="color:#00CCBC">
+        OUR MENU:
+    </h1>
+    <div class="menu d-flex flex-wrap col-12">
 
     <div v-for="plate in singleRestaurant.restaurant.plates" class=" mx-4 my-4">
-      <div style="width: 250px; border:1px solid rgba(221, 218, 218, 0.753)" class="plate-card">
+                <div style="width: 250px; border:1px solid rgba(221, 218, 218, 0.753)" class="plate-card">
+                    <img v-if="plate.image.startsWith('http')" :src="plate.image" alt="immagine">
+                    <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.image" alt="immagine">
+                </div>
 
-        <img v-if="plate.image.startsWith('http')" :src="plate.image" alt="immagine">
-        <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.image" alt="immagine">
-                  <!-- <img :src="plate.image" :alt="plate.id"> -->
-      </div>
-  
-  
-              <div class="card-body flex-column px-2">
-                  <h6 style="color:#262c2cf7" class="fw-bolder">{{ plate['name']  }}</h6>
-  
-                  <p class="card-text mb-1" style="color:#4d7c1be2;"> {{ plate.description }}</p>
-                  <p class="card-text" style="color:rgba(119, 136, 153, 0.793);"> € {{ plate.price  }}</p>
-                  <p v-if="plate.visible == 1">Visibile</p>
-                  <p v-else>Non visibile</p>
+                <div class="card-body flex-column px-2">
+                    <h6 style="color:#262c2cf7" class="fw-bolder">{{ plate['name']  }}</h6>
+                    <p class="card-text mb-1" style="color:#4d7c1be2;"> {{ plate.description }}</p>
+                    <p class="card-text" style="color:rgba(119, 136, 153, 0.793);"> € {{ plate.price  }}</p>
+                    <p v-if="plate.visible == 1">Visibile</p>
+                    <p v-else>Non visibile</p>
                 </div>
                 <button @click="addToCart(plate)" style="background-color:#00CCBC; border-radius:25px;" class="px-3 py-1">
-                  Acquista
+                    Acquista
                 </button>
-  
-  
     </div>
 
     </div>
@@ -162,8 +154,8 @@ export default {
                     
                         <article class="product-card">
                             <div class="product-img me-4">
-                              <img v-if="plate.plate.image.startsWith('http')" :src="plate.plate.image" alt="immagine">
-                              <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.plate.image" alt="immagine">
+                                <img v-if="plate.plate.image.startsWith('http')" :src="plate.plate.image" alt="immagine">
+                                <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.plate.image" alt="immagine">
                             </div>
                             
                                 <div class="description">
@@ -242,37 +234,34 @@ export default {
                             </div>
                             <div class="fw-bold">
 
-                              <ul>
-                                <li class="fs-3">
-                                  {{ totalPrice + deliveryFee + tip }} €
-                                </li>
-                              </ul>
+                                <ul>
+                                    <li class="fs-3">
+                                    {{ totalPrice + deliveryFee + tip }} €
+                                    </li>
+                                </ul>
 
                             </div>
                         </div>
 
-            <router-link :to="{ name: 'AppPagamenti'}"> 
-              <button class="btn btn-success py-2 fs-4 mt-3">
-                  Checkout Now
-              </button>
-            </router-link>
+                            <router-link :to="{ name: 'AppPagamenti'}"> 
+                                <button class="btn btn-success py-2 fs-4 mt-3">
+                                    Checkout Now
+                                </button>
+                            </router-link>
                         </div>
                         </div>
             </div>
         </div>
     </div>
 
-      </div>
+    </div>
 </template>
 
 
 <style scoped>
-
-<<<<<<< HEAD
-=======
 .carrello{
 
- .background{
+    .background{
     font-family: "Metrophobic, Sans Serif";
     background-color: #eee;
     padding: 3rem 5rem;
@@ -348,14 +337,14 @@ export default {
     }
     hr {
         margin-bottom: 1.3rem;
-       border: 1px dashed #00CCBC;
+        border: 1px dashed #00CCBC;
     }
     .subtotal,
     .amount-due{
         font-size: 1.25rem;
         div ul {
             padding: 0;
-           li{
+            li{
             list-style-type: none;
             margin-bottom: 0.8rem;
         } 
@@ -384,13 +373,12 @@ export default {
 @media (max-width: 768px) {
     .background {
         padding: 1rem 1rem;
-       .left-size .product-card{
+        .left-size .product-card{
         .product-img{
         width: 50%;
         }
     }  
     }
-  
 } 
 @media (max-width: 576px) {
     .background .left-size .product-card{
@@ -399,9 +387,8 @@ export default {
         width: 100%;
         }
     } 
-  
+
 }
 }
->>>>>>> carrello
 
 </style>
