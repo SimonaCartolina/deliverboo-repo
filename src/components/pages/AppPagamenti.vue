8d2428196ba81d1516985d8b57b1be7095dacf12
Simona Cartolina <simonacartolina@hotmail.com>
2023-09-28 19:26:41 +0200

    made carrello and pagamenti page

---
+++ src/components/pages/AppPagamenti.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default{
+    name:'AppPagamenti',
+    data(){
+        
+    }
+}
+
+
+</script>
+
+<template>
+
+    <h1>
+        PAGAMENTI
+    </h1>
+
+
+</template>
+
+<style scoped>
+
+</style>
--- src/components/pages/SingleRestaurant.vue
+++ src/components/pages/SingleRestaurant.vue
@@ -5,14 +5,29 @@ export default {
   name: "SingleRestaurant",
   data(){
     return{
+
       singleRestaurant: [],
       cart: [], 
       restaurantMenu: [],
       apiUrl:"",
+      tip: 0,
     }
   },
 
+  computed: {
+    totalPrice() {
+      return this.cart.reduce((total, item) => {
+        return total + item.plate.price * item.quantity;
+      }, 0);
+    },
+
+    deliveryFee() {
+      return this.totalPrice * 0.02;
+    },
+  },
+
   methods: {
+
     getSingleRestaurant() {
 
       const id = this.$route.params.id;
@@ -31,35 +46,50 @@ export default {
     },
 
     addToCart(plate) {
-                        const existingItem = this.cart.find((plate) => plate.id);
-                        if (existingItem) {
-                            existingItem.quantity++; // Aumenta la quantità se il piatto è già nel carrello
-                        } else {
-                            this.cart.push({ plate, quantity: 1 }); // Aggiungi il piatto al carrello
-                        }
-                        },
+      const existingItem = this.cart.find((item) => item.plate.id === plate.id);
+      if (existingItem) {
+              existingItem.quantity++;
+              console.log(existingItem); // Aumenta la quantità se il piatto è già nel carrello
+      } else {
+              this.cart.push({ plate, quantity: 1 });
+              console.log(plate) // Aggiungi il piatto al carrello
+      }
+      },
+                        
+      removeFromCart(plate) {
+        const index = this.cart.findIndex(item => item.plate.id === plate.plate.id);
+
+        if (index !== -1) {
+          this.cart.splice(index, 1); // Remove the plate from the cart
+        }
+      },
 
 
-        removeFromCart(plate) {
-            const index = this.cart.findIndex((plate) => plate.id);
-            if (index !== -1) {
-                if (this.cart[index].quantity > 1) {
-                this.cart[index].quantity--; // Diminuisci la quantità se è maggiore di 1
-                } else {
-                this.cart.splice(index, 1); // Rimuovi completamente il piatto dal carrello se la quantità è 1
-                }
-            }
-    }
-  },
-  created() {
-    this.getSingleRestaurant();
-  },
+
+        increaseQuantity(plate) {
+            plate.quantity++;
+        },
+
+        decreaseQuantity(plate) {
+            if (plate.quantity > 1) {
+              plate.quantity--;
+          }
+        },
+        
+      },
+      
+      created() {
+        this.getSingleRestaurant();
+      }
+
 };
+
+
 </script>
 
 <template>
 
-  <div class="SingleRestaurant d-flex m-5">
+  <div class="SingleRestaurant d-flex m-5 mb-5">
 
             <div class="immagine-profile my-4 col-6">
 
@@ -110,7 +140,257 @@ export default {
     </div>
 
     </div>
+
+
+    <div class="carrello my-5 py-5">
+        <div class="background">
+        <div class="container-fluid">
+            <div class="row d-flex">
+                <h2 class="p-0 mb-3 fw-bolder">Shooping Cart</h2>
+                
+                <div class="left-size col-md-8 col-12">
+
+                    <div v-for="plate in cart" :key="plate" class="d-flex justify-content-between align-items-center">
+                    
+                        <article class="product-card">
+                            <div class="product-img me-4">
+                              <img v-if="plate.plate.image.startsWith('http')" :src="plate.plate.image" alt="immagine">
+                              <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.plate.image" alt="immagine">
+                            </div>
+                            
+                                <div class="description">
+                                    <h3>{{ plate.plate.name }}</h3>
+                                    <p class="price"> € {{ plate.plate.price }}</p>      
+                                    <p class="available fsz">Disponibilità immediata</p>                     
+                                    <p class="fsz">Idoneo alla <strong>Spedizione GRATUITA</strong></p>  
+
+                                    <div class="count d-flex align-items-center">
+                                        <div class="me-3">
+                                            <span>Quantità:{{ plate.quantity }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                        </article>
+
+                        <div>
+                            <button @click="increaseQuantity(plate)" class="mx-2 my-1 bracco">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" style="fill:green">
+                                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                            </button>
+                            <button @click="decreaseQuantity(plate)" class="mx-2 my-1 bracco">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" style="fill:yellow">
+                                <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
+                            
+                            </button>
+                            <button @click="removeFromCart(plate)" class="mx-2 my-1 bracco">
+                                <svg xmlns="http://www.w3.org/2000/svg" style="fill:red" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+                            </button>
+
+                        </div>
+                        
+
+                    </div>
+                </div>
+
+
+                <div class="right-size col-md-4 col-12">
+                    <div class="order-summary">
+                        <div class="pb-3 fs-3">Order Summary</div>
+                        <div class="pb-2 fs-5">Promo Code</div>
+                        <div class="promo-code-button">
+                            <input class="left-promo-input px-2 py-1 fs-4 mb-4" placeholder="Enter your promo code">
+                            <button class="right-promo-button px-2 py-1 fs-4 mb-4">Applica</button>
+                        </div>
+                        <hr>
+                        <div class="subtotal d-flex justify-content-between">
+                            <div>
+                                <ul>
+                                    <li>Subtotal</li>
+                                    <li>Extimated Shipping</li>
+                                    
+                                    <li>Order Total</li>
+
+                                    <label for="tip"> Mancia Rider</label>
+                                    <input type="number" id="tip" v-model="tip" placeholder="Inserisci mancia per il rider" />
+                                </ul> 
+                            </div>
+                            <div class="fw-bold">
+                                <ul>
+                                    <li>{{ totalPrice }} €</li>
+                                    <li>{{ deliveryFee }} €</li>
+                                    <li>{{ totalPrice + deliveryFee }} €</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <hr>        
+                                
+                        <div class="amount-due d-flex justify-content-between">
+                            <div>
+                                <ul>
+                                    <li class="fs-3">
+                                        Amount due
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="fw-bold">
+
+                              <ul>
+                                <li class="fs-3">
+                                  {{ totalPrice + deliveryFee + tip }} €
+                                </li>
+                              </ul>
+
+                            </div>
+                        </div>
+
+            <router-link :to="{ name: 'AppPagamenti'}"> 
+              <button class="btn btn-success py-2 fs-4 mt-3">
+                  Checkout Now
+              </button>
+            </router-link>
+                        </div>
+                        </div>
+            </div>
+        </div>
+    </div>
+
+      </div>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+
+.carrello{
+
+ .background{
+    font-family: "Metrophobic, Sans Serif";
+    background-color: #eee;
+    padding: 3rem 5rem;
+
+    .row>*{
+        margin: 0;
+        padding: 0;
+    }
+    
+    .left-size{
+        height: 680px;
+        overflow-y: scroll;
+        
+        .product-card{
+            display: flex;     
+            padding: .8rem 0;           
+            border-top: 1px solid #00CCBC;
+            .product-img{
+
+                width: 20%;
+                
+                img{
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    border-radius: 15px;
+                }
+            }
+            .description>*{
+                margin: 0;
+                padding: 0;
+            }
+            .available{
+                color: green;
+            }
+            .price{
+                font-weight: bolder;
+                font-size: 1.2rem;
+            }  
+            .fsz{
+                font-size: 0.8rem;
+            }    
+            }
+        }
+        
+    }
+    .right-size{ 
+        .order-summary{
+            border: 1px solid #00CCBC;
+            background-color: #fff;
+            padding: 2rem 1.5rem;
+        }
+        .promo-code-button{
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+        }
+        .left-promo-input{
+            color: black;
+            background-color: #fff;
+            width: 50%;
+        }
+        .right-promo-button{
+            background-color: #00CCBC;
+            border: 2px solid #00CCBC;
+            color: #fff;
+            width: 47%;
+            &:hover{
+                border: 2px solid black;
+            }
+        }
+        
+    }
+    hr {
+        margin-bottom: 1.3rem;
+       border: 1px dashed #00CCBC;
+    }
+    .subtotal,
+    .amount-due{
+        font-size: 1.25rem;
+        div ul {
+            padding: 0;
+           li{
+            list-style-type: none;
+            margin-bottom: 0.8rem;
+        } 
+        }      
+    }
+    .btn-success{
+        width: 100%;
+        color: #fff;
+    }
+    .btn-light{
+        width: 100%;
+        border: 1px solid black;
+    }
+    .minus,
+    .plus{
+        cursor: pointer;
+        color: #00CCBC;
+        font-size: 1.2rem;
+        font-weight: bolder;
+        padding: 2px 10px;
+        border: 3px solid #00CCBC;
+        margin: 0 2px;                 
+    }
+
+
+@media (max-width: 768px) {
+    .background {
+        padding: 1rem 1rem;
+       .left-size .product-card{
+        .product-img{
+        width: 50%;
+        }
+    }  
+    }
+  
+} 
+@media (max-width: 576px) {
+    .background .left-size .product-card{
+        display: block;
+        .product-img{
+        width: 100%;
+        }
+    } 
+  
+}
+}
+
+</style>
