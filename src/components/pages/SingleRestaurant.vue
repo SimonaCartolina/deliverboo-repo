58f3f02e199ded9190a9e25e07ece101b4fa8862
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-09-29 16:18:56 +0200

    change visible of the plates

--- src/components/pages/SingleRestaurant.vue
+++ src/components/pages/SingleRestaurant.vue
@@ -88,34 +88,69 @@ export default {
   <div class="menu d-flex flex-wrap col-12">
 
     <div v-for="plate in singleRestaurant.restaurant.plates" class=" mx-4 my-4">
-      <div style="width: 250px; border:1px solid rgba(221, 218, 218, 0.753)" class="plate-card">
 
-        <img v-if="plate.image.startsWith('http')" :src="plate.image" alt="immagine">
-        <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.image" alt="immagine">
-                  <!-- <img :src="plate.image" :alt="plate.id"> -->
-      </div>
-  
-  
-              <div class="card-body flex-column px-2">
-                  <h6 style="color:#262c2cf7" class="fw-bolder">{{ plate['name']  }}</h6>
-  
-                  <p class="card-text mb-1" style="color:#4d7c1be2;"> {{ plate.description }}</p>
-                  <p class="card-text" style="color:rgba(119, 136, 153, 0.793);"> € {{ plate.price  }}</p>
-                  <p v-if="plate.visible == 1">Visibile</p>
-                  <p v-else>Non visibile</p>
+                <div v-if="plate.visible == 1">
+
+                    <div style="width: 250px; border:1px solid rgba(221, 218, 218, 0.753)" class="plate-card">
+
+                    <img v-if="plate.image.startsWith('http')" :src="plate.image" alt="immagine">
+                    <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.image" alt="immagine">
+                            
+                    </div>
+
+
+                    <div class="card-body flex-column px-2">
+                      <h6 style="color:#262c2cf7" class="fw-bolder">{{ plate['name']  }}</h6>
+
+                      <p class="card-text mb-1" style="color:#4d7c1be2;"> {{ plate.description }}</p>
+                      <p class="card-text" style="color:rgba(119, 136, 153, 0.793);"> € {{ plate.price  }}</p>
+                      
+                    </div>
+                    <button @click="addToCart(plate)" style="background-color:#00CCBC; border-radius:25px;" class="px-3 py-1">
+                      Acquista
+                    </button>
                 </div>
-                <button @click="addToCart(plate)" style="background-color:#00CCBC; border-radius:25px;" class="px-3 py-1">
-                  Acquista
-                </button>
-  
-  
+                <div v-else>
+                  <div class="filter">
+                    <span>No disponibile</span>
+                      <div style="width: 250px; border:1px solid rgba(221, 218, 218, 0.753)" class="plate-card">
+
+                        <img v-if="plate.image.startsWith('http')" :src="plate.image" alt="immagine">
+                        <img v-else :src="'http://127.0.0.1:8000/storage/' + plate.image" alt="immagine">
+                                
+                      </div>
+                      <div class="card-body flex-column px-2">
+                          <h6 style="color:#262c2cf7" class="fw-bolder">{{ plate['name']  }}</h6>
+
+                          <p class="card-text mb-1" style="color:#4d7c1be2;"> {{ plate.description }}</p>
+                          <p class="card-text" style="color:rgba(119, 136, 153, 0.793);"> € {{ plate.price  }}</p>
+                          
+                      </div>
+                      
+                  </div>
+                </div>
     </div>
 
-    </div>
+  </div>
 </template>
 
 
 <style scoped>
+
+.filter{
+  z-index: 1;
+  opacity: 0.2;
+  color: black;
+  position: relative;
+  & span{
+    font-size: 1.2rem;
+    position: absolute;
+    font-weight: bolder;
+    z-index: 2;
+    left: 30%;
+
+  }
+}
 
 
 </style>
